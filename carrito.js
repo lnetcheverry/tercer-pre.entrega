2443daaf9b749fd95fd447af664bfd34a0671f23
lnetcheverry} <leo.etcheverry16@gmail.com>
2023-03-08 18:10:43 -0300

    correcciones

---
+++ carrito.js
@@ -0,0 +1,78 @@
+//Mostrar el carrito de compras: 
+
+const contenedorCarrito = document.getElementById("contenedorCarrito");
+const verCarrito = document.getElementById("verCarrito");
+
+verCarrito.addEventListener("click", () => {
+    mostrarCarrito();
+})
+
+const mostrarCarrito = () => {
+    contenedorCarrito.innerHTML = "";
+    carrito.forEach(producto => {
+        const card = document.createElement("div");
+        card.classList.add("col-xl-3", "col-md-6", "col-sm-12");
+        card.innerHTML = `
+                <div class = "card" >
+                    <img src = "${producto.img}" class = "card-img-tom imgProductos">    
+                    <div class = "card-body" >
+                        <h2> ${producto.nombre} </h2>
+                        <p> ${producto.precio} </p>
+                        <p> ${producto.cantidad} </p>
+                        <button class = "btn colorBoton" id="eliminar${producto.id}" > Eliminar</button>
+                    </div>
+                </div>`
+
+        contenedorCarrito.appendChild(card);
+
+        //Eliminar productos del carrito: 
+
+        const boton = document.getElementById(`eliminar${producto.id}`);
+        boton.addEventListener("click", () => {
+            eliminarDelCarrito(producto.id);
+        })
+    })
+    calcularTotal();
+}
+
+//Función que elimina el producto del carrito: 
+
+const eliminarDelCarrito = (id) => {
+    const producto = carrito.find(producto => producto.id === id);
+    const indice = carrito.indexOf(producto);
+    carrito.splice(indice,1);
+    mostrarCarrito();
+
+    //LocalStorage: 
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+//Vaciamos todo el carrito de compras. 
+
+const vaciarCarrito = document.getElementById("vaciarCarrito");
+
+vaciarCarrito.addEventListener("click", () => {
+    eliminarTodoElCarrito();
+})
+
+const eliminarTodoElCarrito = () => {
+    carrito = [];
+    mostrarCarrito();
+
+    //LocalStorage: 
+    localStorage.clear();
+}
+
+
+//Mostrar un mensaje con el total de la compra.
+
+const total = document.getElementById("total");
+
+const calcularTotal = () => {
+    let totalCompra = 0; 
+    carrito.forEach( producto => {
+        totalCompra += producto.precio * producto.cantidad;
+        //+= es igual a poner totalCompra = totalCompra + producto.precio * producto.cantidad. 
+    })
+    total.innerHTML = `Total $${totalCompra}`;
+}
